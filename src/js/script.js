b4f4f787dafe65a56287fecc9f6443713fdfde67
Aristarh Gran <awebdeff@gmail.com>
2023-01-20 22:34:52 +0300

    добавил карту, настроил ее, сделал рефакторинг css

--- src/js/script.js
+++ src/js/script.js
@@ -12,5 +12,30 @@ window.addEventListener('DOMContentLoaded', () => {
 	textarea.addEventListener('keyup', countLetters)
 })
 
+// яндекс-карта
+let center = [55.727142, 37.469345]
+
+function init() {
+	let map = new ymaps.Map('map', {
+		center: center,
+		zoom: 10
+	})
+
+	map.controls.remove('typeSelector'); // удаляем тип
+	map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+	map.controls.remove('rulerControl'); // удаляем контрол правил
+
+	let placemark = new ymaps.Placemark(center, {}, {
+		iconLayout: 'default#image',
+		iconImageHref: 'build/img/mark.png',
+		iconImageSize: [40, 40],
+		iconImageOffset: [0, 0]
+	})
+
+	map.geoObjects.add(placemark)
+}
+
+ymaps.ready(init)
+
 
 
