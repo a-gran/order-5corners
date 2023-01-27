1d33e87d071615d4e8e978750de251a93dc9e59f
Aristarh Gran <awebdeff@gmail.com>
2023-01-27 21:04:15 +0300

    связал поле адресной строки и яндекс-карту по API, наконец-то!

--- build/js/main.min.js
+++ build/js/main.min.js
@@ -1,123 +1,120 @@
-window.addEventListener('DOMContentLoaded', () => {
-	const goodCards = document.querySelectorAll('.good-card')
+//window.addEventListener('DOMContentLoaded', () => {
+const goodCards = document.querySelectorAll('.good-card')
 
-	goodCards.forEach((item) => {
-		const decrement = item.querySelector('.decrement')
-		const increment = item.querySelector('.increment')
-		let costOne = +item.querySelector('.good-cost__final-for-one').innerText
-		let costAll = +item.querySelector('.good-cost__final-for-all').innerText
+goodCards.forEach((item) => {
+	const decrement = item.querySelector('.decrement')
+	const increment = item.querySelector('.increment')
+	let costOne = +item.querySelector('.good-cost__final-for-one').innerText
+	let costAll = +item.querySelector('.good-cost__final-for-all').innerText
 
-		const quontityBox = item.querySelector('.quontity')
-		let quontity = +quontityBox.value
+	const quontityBox = item.querySelector('.quontity')
+	let quontity = +quontityBox.value
 
-		increment.addEventListener('click', () => {
-			if (quontity < 99) {
-				quontity++
-				quontityBox.setAttribute('value', quontity)
-			}
-		})
-
-		decrement.addEventListener('click', () => {
-			if (quontity > 0) {
-				quontity--
-				quontityBox.setAttribute('value', quontity)
-			}
-		})
-
-		costAll = costOne + quontity
-
-		// счетчик символов textarea
-		const textarea = document.querySelector('.contacts-comment__input')
-		const count = document.querySelector('.contacts-comment__count')
-
-		function countLetters() {
-			const text = textarea.value
-			const textlength = textarea.value.length
-			count.innerText = `${textlength}`
+	increment.addEventListener('click', () => {
+		if (quontity < 99) {
+			quontity++
+			quontityBox.setAttribute('value', quontity)
 		}
-
-		textarea.addEventListener('keyup', countLetters)
 	})
 
-	// яндекс-карта
-	let center = [55.727142, 37.469345]
-
-	function init() {
-		let map = new ymaps.Map('map', {
-			center,
-			zoom: 10,
-			controls: ['searchControl']
-		})
-
-		map.controls.remove('typeSelector') // удаляем тип
-		map.controls.remove('zoomControl') // удаляем контрол зуммирования
-		map.behaviors.disable('scrollZoom') // отключение zoom карты
-
-		let placemark = new ymaps.Placemark(center, {}, {
-			iconLayout: 'default#image',
-			iconImageHref: 'build/img/mark.png',
-			iconImageSize: [40, 40],
-			iconImageOffset: [0, 0]
-		})
-
-		map.geoObjects.add(placemark)
-
-		let placeInput = document.querySelector('#address')
-		//let mapBlock = document.querySelector('#map')
-		//let mapSearchField = mapBlock.querySelector('#placeholder + div')
-		//console.log('mapSearchField', mapSearchField)
-		let place = ''
-
-		const handlerEventClick = e => place = placeInput.value
-		const handlerEventEnter = e => {
-			if (e.code === 'Enter') {
-				place = placeInput.value
-				console.log('place', place)
-			}
+	decrement.addEventListener('click', () => {
+		if (quontity > 0) {
+			quontity--
+			quontityBox.setAttribute('value', quontity)
 		}
+	})
 
-		placeInput.addEventListener('keydown', handlerEventEnter)
-		placeInput.addEventListener('click', handlerEventClick)
+	costAll = costOne + quontity
 
-		let searchControl = new ymaps.control.SearchControl({
-			options: {
-				provider: 'yandex#map'
-			}
-		})
+	// счетчик символов textarea
+	const textarea = document.querySelector('.contacts-comment__input')
+	const count = document.querySelector('.contacts-comment__count')
 
-		searchControl.search(place)
-
-		//let myGeocoder = ymaps.geocode(place)
-		//myGeocoder.then(function (res) {
-		//	map.geoObjects.add(res.geoObjects)
-		//})
+	function countLetters() {
+		const text = textarea.value
+		const textlength = textarea.value.length
+		count.innerText = `${textlength}`
 	}
 
-	ymaps.ready(init)
-
-	// dadata
-	const token = '54c558e0a89f381ec441eec29c6cd14318d16c6f'
-
-	$('#address').suggestions({
-		token,
-		type: 'ADDRESS'
-	})
-
-	$('#email').suggestions({
-		token,
-		type: 'EMAIL'
-	})
-
-	$('#name').suggestions({
-		token,
-		type: 'NAME'
-	})
-
-	$('#surname').suggestions({
-		token,
-		type: 'NAME'
-	})
+	textarea.addEventListener('keyup', countLetters)
 })
+
+// яндекс-карта
+let init = () => {
+	let center = [55.727142, 37.469345]
+	let place
+
+	let map = new ymaps.Map('map', {
+		center,
+		zoom: 11
+	})
+
+	// адресная строка
+	let placeInput = document.querySelector('#address')
+
+	// создадим экземпляр элемента управления «поиск по карте»
+	// с установленной опцией провайдера данных для поиска по организациям
+	let searchControl = new ymaps.control.SearchControl({
+		options: {
+			provider: 'yandex#search'
+		}
+	})
+
+	placeInput.addEventListener('keydown', e => {
+		if (e.code === 'Enter' && place != '') {
+			place = placeInput.value
+
+			searchControl.search(`${place}`)
+
+			// инициализация метки для карты
+			let placemark = new ymaps.Placemark(map.getCenter(`${place}`), {}, {
+				iconLayout: 'default#image',
+				iconImageHref: 'build/img/mark.png',
+				iconImageSize: [30, 30],
+				iconImageOffset: [0, 0]
+			})
+
+			// добавим метку на карту
+			map.geoObjects.add(placemark)
+		}
+	})
+
+	map.controls.add(searchControl)
+
+	// удаляем различные исходные элементы карты
+	map.controls.remove('typeSelector') // удаляем тип
+	map.controls.remove('zoomControl')  // удаляем контрол зуммирования
+	map.controls.remove('geolocationControl') // удаляем геолокацию
+	map.controls.remove('trafficControl') // удаляем контроль трафика
+	map.controls.remove('fullscreenControl') // удаляем кнопку перехода в полноэкранный режим
+	map.controls.remove('rulerControl'); // удаляем контрол правил
+}
+
+ymaps.ready(init)
+
+// dadata
+const token = '54c558e0a89f381ec441eec29c6cd14318d16c6f'
+
+$('#address').suggestions({
+	token,
+	type: 'ADDRESS'
+})
+
+$('#email').suggestions({
+	token,
+	type: 'EMAIL'
+})
+
+$('#name').suggestions({
+	token,
+	type: 'NAME'
+})
+
+$('#surname').suggestions({
+	token,
+	type: 'NAME'
+})
+//})
 
 
 
